4585b6f25c70dbaeb1a8018130ab1a909f063993
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-06-03 12:10:08 +0300

    Removed customVector.cpp and changes in customString.h/.cpp and in customVector.h

--- customFunctions/customString.cpp
+++ customFunctions/customString.cpp
@@ -1,62 +1,220 @@
+// Felipe Urra Rivadeneira 0MI8000066
+
 #include "customString.h"
-#include <iostream>
-#include <cstring>
+#include "cstring"
+#include <stdexcept>
 
-void CustomString::copyFrom(const CustomString& other) {
-    setData(other.data);
+CustomString::CustomString() : CustomString("")
+{
 }
 
-void CustomString::free() {
-    delete[] data;
-    data = nullptr;
+CustomString::CustomString(const char* data) : size(strlen(data)), capacity(allocateCapacity(size))
+{
+	this->data = new char[this->capacity];
+	strcpy(this->data, data);
 }
 
-void CustomString::setData(const char* data) {
-    if (!data || this->data == data) return;
-
-    this->data = new char[strlen(data) + 1];
-    strcpy(this->data, data);
+CustomString::CustomString(size_t newSize) : size(0), capacity(allocateCapacity(newSize))
+{
+	this->data = new char[this->capacity] { '\0' };
 }
 
-const char* CustomString::getData() const{
-    return data;
+CustomString::CustomString(const CustomString& other)
+{
+	copyFrom(other);
 }
 
-// big four
-CustomString::CustomString() : data(nullptr) {};
+CustomString& CustomString::operator=(const CustomString& other)
+{
+	if (this != &other)
+	{
+		free();
+		copyFrom(other);
+	}
 
-CustomString::CustomString(const CustomString& other) {
-    copyFrom(other);
+	return *this;
 }
 
-CustomString& CustomString::operator=(const CustomString& other) {
-    if (this != &other)
-    {
-        free();
-        copyFrom(other);   
+const char* CustomString::c_str() const
+{
+	return this->data;
+}
+
+size_t CustomString::getSize() const
+{
+	return this->size;
+}
+
+size_t CustomString::getCapacity() const
+{
+	return this->capacity;
+}
+
+// Serialize/deserialize
+void CustomString::serialize(std::ofstream& out) const {
+	int len = this->size;
+	out.write(reinterpret_cast<const char*>(&len), sizeof(len));
+	out.write(this->data, len);
+}
+void CustomString::deserialize(std::ifstream& in) {
+	int len;
+	in.read(reinterpret_cast<char*>(&len), sizeof(len));
+	if (len < 0 || len > 10000) {
+        throw std::runtime_error("String length out of bounds");
     }
-    return *this;
-}
 
-CustomString::~CustomString() {
-    free();
-}
-
-
-
-CustomString::CustomString(const char* data) {
-    setData(data);
+	delete[] this->data;
+	this->data = new char[len + 1];
+	in.read(this->data, len);
+	this->data[len] = '\0';
+	this->size = len;
+	this->capacity = len + 1;
 }
 
 
-CustomString& CustomString::operator=(const char* str) {
-    setData(data);
+const char& CustomString::operator[](size_t idx) const
+{
+	return this->data[idx];
 }
 
-bool operator==(const CustomString& lStr, const CustomString& rStr) {
-    return strcmp(lStr.data, rStr.data) == 0;
+char& CustomString::operator[](size_t idx)
+{
+	return this->data[idx];
 }
 
-bool operator!=(const CustomString& lStr, const CustomString& rStr) {
-    return strcmp(lStr.data, rStr.data) != 0;
+CustomString& CustomString::operator+=(const CustomString& other)
+{
+	if (getSize() + other.getSize() >= getCapacity())
+	{
+		resize(allocateCapacity(getSize() + other.getSize()));
+	}
+
+	strncat(this->data, other.data, other.getSize());
+
+	this->size += other.getSize();
+
+	return *this;
+}
+
+CustomString operator+(const CustomString& lhs, const CustomString& rhs)
+{
+	CustomString toReturn(lhs.getSize() + rhs.getSize());
+
+	toReturn += lhs;
+	toReturn += rhs;
+
+	return toReturn;
+}
+
+CustomString CustomString::substr(size_t begin, size_t howMany) const
+{
+	if (begin + howMany > getSize()) return CustomString("");
+
+	CustomString toReturn(howMany);
+	strncat(toReturn.data, this->data + begin, howMany);
+	return toReturn;
+}
+
+bool operator==(const CustomString& lhs, const CustomString& rhs)
+{
+	return strcmp(lhs.data, rhs.data) == 0;
+}
+
+bool operator!=(const CustomString& lhs, const CustomString& rhs)
+{
+	return strcmp(lhs.data, rhs.data) != 0;
+}
+
+bool operator<=(const CustomString& lhs, const CustomString& rhs)
+{
+	return strcmp(lhs.data, rhs.data) <= 0;
+}
+
+bool operator>=(const CustomString& lhs, const CustomString& rhs)
+{
+	return strcmp(lhs.data, rhs.data) >= 0;
+}
+
+bool operator<(const CustomString& lhs, const CustomString& rhs)
+{
+	return strcmp(lhs.data, rhs.data) < 0;
+}
+
+bool operator>(const CustomString& lhs, const CustomString& rhs)
+{
+	return strcmp(lhs.data, rhs.data) > 0;
+}
+
+std::istream& operator>>(std::istream& is, CustomString& other)
+{
+	char buff[Constants::MAX_SIZE + 1];
+	is >> buff;
+
+	size_t buffLength = strlen(buff);
+
+	if (buffLength >= other.getCapacity())
+	{
+		other.resize(other.allocateCapacity(buffLength));
+	}
+
+	strcpy(other.data, buff);
+
+	other.size = buffLength;
+
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const CustomString& other)
+{
+	return os << other.data;
+}
+
+CustomString::~CustomString()
+{
+	free();
+}
+
+unsigned int CustomString::getNextPowerOfTwo(unsigned int n) const
+{
+	if (n == 0) return 1;
+
+	while (n & (n - 1))
+	{
+		n &= (n - 1);
+	}
+
+	return n << 1;
+}
+
+unsigned int CustomString::allocateCapacity(unsigned int size) const
+{
+	return std::max(getNextPowerOfTwo(size + 1), 8u);
+}
+
+void CustomString::resize(size_t newCapacity)
+{
+	this->capacity = newCapacity;
+
+	char* newData = new char[this->capacity];
+	strcpy(newData, this->data);
+
+	delete[] this->data;
+	this->data = newData;
+}
+
+void CustomString::copyFrom(const CustomString& other)
+{
+	this->size = other.size;
+	this->capacity = other.capacity;
+
+	this->data = new char[strlen(other.data) + 1];
+	strcpy(this->data, other.data);
+}
+
+void CustomString::free()
+{
+	delete[] this->data;
+
+	this->data = nullptr;
+	this->size = this->capacity = 0;
 }
--- customFunctions/customString.h
+++ customFunctions/customString.h
@@ -1,27 +1,67 @@
+// Felipe Urra Rivadeneira 0MI8000066
+
 #pragma once
+
+#include <iostream>
+#include <fstream>
+
+namespace Constants
+{
+	constexpr size_t MAX_SIZE = 1024;
+}
 
 class CustomString
 {
 private:
-    char* data;
+	char* data;
+	size_t size;
+	size_t capacity;
 
-    void copyFrom(const CustomString&);
-    void free();
+	explicit CustomString(size_t newSize);
+
+	unsigned int getNextPowerOfTwo(unsigned int n) const;
+	unsigned int allocateCapacity(unsigned int size) const;
+
+	void resize(size_t newCapacity);
+
+	void copyFrom(const CustomString& other);
+	void free();
 
 public:
-    // big four
-    CustomString();
-    CustomString(const CustomString&);
-    CustomString& operator=(const CustomString&);
-    ~CustomString();
+	CustomString();
+	CustomString(const char* data);
 
-    CustomString(const char*);
+	CustomString(const CustomString& other);
+	CustomString& operator=(const CustomString& other);
 
-    CustomString& operator=(const char*);
-    friend bool operator==(const CustomString&, const CustomString&);
-    friend bool operator!=(const CustomString&, const CustomString&);
+	const char* c_str() const;
 
-    // getters and setters
-    const char* getData() const;
-    void setData(const char*);
+	size_t getSize() const;
+	size_t getCapacity() const;
+
+	// Serialize/deserialize
+	void serialize(std::ofstream& out) const;
+    void deserialize(std::ifstream& in);
+
+	const char& operator[](size_t idx) const;
+	char& operator[](size_t idx);
+
+	CustomString& operator+=(const CustomString& other);
+	friend CustomString operator+(const CustomString& lhs, const CustomString& rhs);
+
+	CustomString substr(size_t begin, size_t howMany) const;
+
+	friend bool operator==(const CustomString& lhs, const CustomString& rhs);
+	friend bool operator!=(const CustomString& lhs, const CustomString& rhs);
+
+	friend bool operator<=(const CustomString& lhs, const CustomString& rhs);
+	friend bool operator>=(const CustomString& lhs, const CustomString& rhs);
+
+	friend bool operator<(const CustomString& lhs, const CustomString& rhs);
+	friend bool operator>(const CustomString& lhs, const CustomString& rhs);
+
+	friend std::istream& operator>>(std::istream& is, CustomString& other);
+	friend std::ostream& operator<<(std::ostream& os, const CustomString& other);
+
+	~CustomString();
 };
